f0ac9bfe48e9ac554be39e51f38e6a72279b5df5
Sam Johnson <s_johnso@yahoo.com>
2023-07-16 18:34:13 -0500

    cleaned up formatting

--- styles/script.js
+++ styles/script.js
@@ -4,14 +4,14 @@ let totalMonthlySalary = 0;
 const numFor = Intl.NumberFormat('en-US');
 
 function onReady() {
-// submit button listener
-$('#submit-button').on('click', handleSubmit);
+  // submit button listener
+  $('#submit-button').on('click', handleSubmit);
 
-// delete button listener
-$('#salaryTableBody').on('click', '#delete-button', handleDelete);
-
+  // delete button listener
+  $('#salaryTableBody').on('click', '#delete-button', handleDelete);
 }
 
+// function that calls when submit is clicked
 function handleSubmit(event) {
   event.preventDefault();
 
@@ -21,8 +21,6 @@ function handleSubmit(event) {
   const idNumberEntry = $('#idNumber').val();
   const jobTitleEntry = $('#jobTitle').val();
   const salaryEntry = $('#salary').val();
-
-  // console.log(firstNameEntry, lastNameEntry, idNumberEntry, jobTitleEntry, salaryEntry);
 
   // return input boxes back to placeholder text
   $('input').val('');
@@ -40,20 +38,21 @@ function handleSubmit(event) {
   `)
 
   // calculate monthly salary and add to totalMonthlySalary
-  totalMonthlySalary += Number(salaryEntry.replaceAll(",", ""))/12;
+  totalMonthlySalary += Number(salaryEntry.replaceAll(",", "")) / 12;
+
   // round to 2 decimal places
-  totalMonthlySalary = totalMonthlySalary.toFixed(2);
+  const roundMonthlySalary = totalMonthlySalary.toFixed(2);
+
   // check if totalMonthlySalary is more than $20,000
-  if (totalMonthlySalary > 20000) {
-    $('#monthlySalary').css('background-color', 'red')
-    }
+  if (roundMonthlySalary > 20000) {
+    $('h3').css({ 'background-color': 'red', 'color': 'yellow' })
+  }
+
   // turn totalMonthlySalary into a string with commas
-  totalMonthlySalary = numFor.format(totalMonthlySalary);
-  console.log('total monthly convert to string after add:', totalMonthlySalary)
+  const stringMonthlySalary = numFor.format(roundMonthlySalary);
+
   // input totalMonthlySalary into monthlySalary
-  $('#monthlySalary').text(totalMonthlySalary);
-  // convert totalMonthly salery back into a number for next input
-  totalMonthlySalary = Number(totalMonthlySalary.replaceAll(",", ""));
+  $('#monthlySalary').text(stringMonthlySalary);
 }
 
 // function to delete rows and subtract salary when remove button is clicked
@@ -62,20 +61,22 @@ function handleDelete() {
   $(this).closest('tr').remove();
 
   // subtract from total monthly salary
-  let toSubtract = $(this).closest('tr').find('td:eq(4)').text();
-  totalMonthlySalary -= Number(toSubtract.replaceAll(",", ""))/12;
-  totalMonthlySalary = totalMonthlySalary.toFixed(2);
-  console.log('after subtract: ', totalMonthlySalary);
-  // check if totalMonthlySalary is less than 20,000
-  if (totalMonthlySalary <= 20000) {
-    $('#monthlySalary').css('background-color', 'darksalmon')
-    }
+
+  const toSubtract = $(this).closest('tr').find('td:eq(4)').text();
+  // find monthly amount to subtract
+  totalMonthlySalary -= Number(toSubtract.replaceAll(",", "")) / 12;
+
+  // round to nearest hundreth
+  const roundMonthlySalary = totalMonthlySalary.toFixed(2);
+
+  // check if totalMonthlySalary is less than 20,000, if so change background back
+  if (roundMonthlySalary <= 20000) {
+    $('h3').css({ 'background-color': 'darksalmon', 'color': '#4b4949' })
+  }
   // turn totalMonthlySalary back into a string with commas
-  totalMonthlySalary = numFor.format(totalMonthlySalary);
-  console.log('total monthly back to string: ', totalMonthlySalary)
+  const stringMonthlySalary = numFor.format(roundMonthlySalary);
+
   // replace monthlySalary with new value
-  $('#monthlySalary').text(totalMonthlySalary);
-  // convert totalMonthly salery back into a number for next input
-  totalMonthlySalary = Number(totalMonthlySalary.replaceAll(",", ""));
+  $('#monthlySalary').text(stringMonthlySalary);
 }
 
