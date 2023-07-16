673072075fbbc77337c990fb23eb286652398d78
Sam Johnson <s_johnso@yahoo.com>
2023-07-16 13:55:48 -0500

    added what to do with comma entries

--- styles/script.js
+++ styles/script.js
@@ -8,6 +8,7 @@ $('#submit-button').on('click', handleSubmit);
 
 // delete button listener
 $('#salaryTableBody').on('click', '#delete-button', handleDelete);
+
 }
 
 function handleSubmit(event) {
@@ -38,25 +39,44 @@ function handleSubmit(event) {
   `)
 
   // calculate monthly salary and add to totalMonthlySalary
-  totalMonthlySalary = totalMonthlySalary + Number(salaryEntry)/12;
-  $('#monthlySalary').text(totalMonthlySalary);
-  // console.log(totalMonthlySalary);
-
+  totalMonthlySalary += Number(salaryEntry.replaceAll(",", ""))/12;
+  // round to 2 decimal places
+  totalMonthlySalary = totalMonthlySalary.toFixed(2);
   // check if totalMonthlySalary is more than $20,000
   if (totalMonthlySalary > 20000) {
     $('#monthlySalary').css('background-color', 'red')
-  }
-
+    }
+  // turn totalMonthlySalary into a string with commas
+  totalMonthlySalary = totalMonthlySalary.toLocaleString("en-US");
+  console.log('total monthly convert to string after add:', totalMonthlySalary)
+  // input totalMonthlySalary into monthlySalary
+  $('#monthlySalary').text(totalMonthlySalary);
+  // convert totalMonthly salery back into a number for next input
+  totalMonthlySalary = Number(totalMonthlySalary.replaceAll(",", ""));
+  console.log(totalMonthlySalary)
 
 }
 
 // function to delete rows and subtract salary when remove button is clicked
 function handleDelete() {
-// remove row
-$(this).closest('tr').remove();
+  // remove row
+  $(this).closest('tr').remove();
 
-// subtract from total monthly salary
-const toSubtract = $(this).closest('tr').find('td:eq(4)').text();
-totalMonthlySalary -= toSubtract/12
-$('#monthlySalary').text(totalMonthlySalary);
+  // subtract from total monthly salary
+  let toSubtract = $(this).closest('tr').find('td:eq(4)').text();
+  totalMonthlySalary -= Number(toSubtract.replaceAll(",", ""))/12;
+  totalMonthlySalary = totalMonthlySalary.toFixed(2);
+  console.log('after subtract: ', totalMonthlySalary);
+  // check if totalMonthlySalary is less than 20,000
+  if (totalMonthlySalary <= 20000) {
+    $('#monthlySalary').css('background-color', 'darksalmon')
+    }
+  // turn totalMonthlySalary back into a string with commas
+  totalMonthlySalary = totalMonthlySalary.toLocaleString("en-US");
+  console.log('total monthly back to string: ', totalMonthlySalary)
+  // replace monthlySalary with new value
+  $('#monthlySalary').text(totalMonthlySalary);
+  // convert totalMonthly salery back into a number for next input
+  totalMonthlySalary = Number(totalMonthlySalary.replaceAll(",", ""));
 }
+
